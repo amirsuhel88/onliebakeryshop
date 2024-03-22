8d4a97eca8838be674eabe0bda89d0e34f3f8c9d
amirsuhel88 <60836385+amirsuhel88@users.noreply.github.com>
2024-03-22 14:49:44 +0530

    product api is created. product can be added, view all products as a admin and can view details of a individual product

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
-const express = require("express")
+const express = require("express");
 const cors = require("cors");
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-const user = require("./routes/userRoute")
+const user = require("./routes/userRoute");
+const product = require("./routes/ProductRoute");
 
-app.use("/api/v1", user)
+
+
+app.use("/api/v1", user);
+app.use("/api/v2", product);
 
 module.exports = app;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,34 @@
 //import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 //import Login from './Login';
-import Login from './Login'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Signup from './Signup';
-//import home from './home'
-
+import Login from "./pages/Login/Login.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup/Signup.js";
+import Home from "./pages/Home/Home.js";
+import Profile from "./pages/Profile/Profile.js";
+import { AuthProvider } from "./context.js/AuthContext.js";
+import Category from "./pages/Category/Category.js";
+import Cart from "./pages/Cart/Cart.js";
 
 function App() {
   return (
-    <BrowserRouter>   
-      <Routes>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/signup' element={<Signup/>}></Route>
-        <Route path='/home' element={<home/>}> </Route>
-      </Routes>
-      {/* <test/> */}
-      
-    </BrowserRouter>
-  )
+    <AuthProvider>
+      <div className="page-container">
+        <div className="conent-wrap">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/login" element={<Login />}></Route>
+              <Route path="/signup" element={<Signup />}></Route>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/profile" element={<Profile />}></Route>
+              <Route path="/category" element={<Category />}></Route>
+              <Route path="/cart" element={<Cart />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </div>
+    </AuthProvider>
+  );
 }
 
 export default App;
